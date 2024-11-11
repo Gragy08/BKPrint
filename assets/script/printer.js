2b2d436b6aafa469fe3ab4a4320fe87a38e35ba7
Gragy08 <giaqui1712@gmail.com>
2024-11-11 11:31:07 +0700

    more responsive add/edit printer page

--- assets/script/printer.js
+++ assets/script/printer.js
@@ -1,3 +1,33 @@
+// // Hiển thị thông báo
+// const showAlert = (content = null, time = 3000, type = "alert--success") => {
+//     if(content) {
+//         const newAlert = document.createElement("div");
+//         newAlert.setAttribute("class", `alert ${type}`);
+    
+//         newAlert.innerHTML = `
+//             <span class="alert__content">${content}</span>
+//             <span class="alert__close">
+//             <i class="fa-solid fa-xmark"></i>
+//             </span>
+//         `;
+    
+//         const alertList = document.querySelector(".alert-list");
+    
+//         alertList.appendChild(newAlert);
+    
+//         const alertClose = newAlert.querySelector(".alert__close");
+        
+//         alertClose.addEventListener("click", () => {
+//             alertList.removeChild(newAlert);
+//         })
+        
+//         setTimeout(() => {
+//             alertList.removeChild(newAlert);
+//         }, time)
+//     }
+// }
+// // Hết Hiển thị thông báo
+
 //Vẽ ra giao diện từ database
 const elementProductList = document.querySelector("#product-list");
 if(elementProductList) {
@@ -31,37 +61,31 @@ if(elementProductList) {
 }
 //Hết Vẽ ra giao diện từ database
 
-// const formCreate = document.querySelector("#form-create");
-// if(formCreate) {
-//     formCreate.addEventListener("submit", (event) => {
-//         event.preventDefault();
+const formCreate = document.querySelector("#form-create");
+if(formCreate) {
+    formCreate.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-//         const code = formCreate.code.value;
-//         const printLaser = formCreate.getElementById("printLaser").checked;
-//         const colorInkjet = formCreate.getElementById("colorInkjet").checked;
-//         const singleSideBW = formCreate.getElementById("singleSideBW").checked;
-//         const doubleSideBW = formCreate.getElementById("doubleSideBW").checked;
-//         const singleSideC = formCreate.getElementById("singleSideC").checked;
-//         const Copy = formCreate.getElementById("Copy").checked;
-//         const scanBW = formCreate.getElementById("scanBW").checked;
-//         const scanC = formCreate.getElementById("scanC").checked;
-//         const speed = formCreate.speed.value;
-//         const active = formCreate.getElementById("active").checked;
-//         const inactive = formCreate.getElementById("inactive").checked;
-//         const description = formCreate.description.value;
+        const code = formCreate.code.value;
+        const printLaser = formCreate.getElementById("printLaser").checked;
+        const colorInkjet = formCreate.getElementById("colorInkjet").checked;
+        const singleSideBW = formCreate.getElementById("singleSideBW").checked;
+        const doubleSideBW = formCreate.getElementById("doubleSideBW").checked;
+        const singleSideC = formCreate.getElementById("singleSideC").checked;
+        const Copy = formCreate.getElementById("Copy").checked;
+        const scanBW = formCreate.getElementById("scanBW").checked;
+        const scanC = formCreate.getElementById("scanC").checked;
+        const speed = formCreate.speed.value;
+        const active = formCreate.getElementById("active").checked;
+        const inactive = formCreate.getElementById("inactive").checked;
+        const description = formCreate.description.value;
 
-//         console.log(code);
-//         console.log(printLaser);
-//         console.log(colorInkjet);
-//         console.log(singleSideBW);
-//         console.log(doubleSideBW);
-//         console.log(singleSideC);
-//         console.log(Copy);
-//         console.log(scanBW);
-//         console.log(scanC);
-//         console.log(speed);
-//         console.log(active);
-//         console.log(inactive);
-//         console.log(description);
-//     })
-// }
+        //In ra cảnh báo nếu chưa nhập hết...
+
+        const data = {
+            code: code,
+            
+        };
+
+    })
+}
